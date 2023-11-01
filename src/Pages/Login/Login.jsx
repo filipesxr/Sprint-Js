318f727af15ae10d29647dc1a86c5bfd91bb2978
filipesxr <rm97830@fiap.com.br>
2023-11-01 15:59:18 -0300

    Finalização da estrutura da validação do login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,18 +1,78 @@
+import { useState } from 'react';
 import './Login.scss';
+import { useNavigate } from 'react-router';
 
 export default function Login(){
+    // UseNavigate para redirecionar
+    const navigate = useNavigate(); 
+
+    // UseState para armazenar os dados do Form
+    const [usuario, setUsuario] = useState({
+        'email':"",
+        'senha':""
+    })
+
+    // Preenchimento do form
+    const handleChange = (e) => {
+        // Destructuring nos campos do Form(name,input)
+        const{name,value} = e.target;
+
+        // Preenchendo o UseState com os valores da desestruturação, utilizando o operador spread.
+        setUsuario({...usuario,[name]:value});
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        let users;
+
+        try {
+            const response = await fetch ('....')
+            users = await response.json()
+        } catch (error) {
+            alert('Ocorreu um erro!')
+        }
+
+        // Realizando a validação do usuário
+        for (let index = 0; index < users.length; index++) {
+            const user = users[index];
+            
+            // Realizando a comparação
+            if (user.email == usuario.email && user.senha == usuario.senha){
+                alert('LogIn realizado!');
+                navigate('/');
+                return;
+            }
+        }
+        
+        alert('LogIn ou senha incorretos!')
+        setUsuario({
+            "email":"",
+            "senha":""
+        })
+    }
     
     return(
         <>
             <h1>LogIn</h1>
 
             <div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <fieldset>
-                        <label htmlFor="idEmail">Email</label>
-                        <input type='email' name='email' id='idEmail' placeholder='seuemail@gmail.com' />
-                        <label htmlFor="idSenha">Senha</label>
-                        <input type='password' name='password' id='idPassword' placeholder='********' />
+                        <legend>Gerenciar Sua Conta</legend>
+                        <div>
+                            <label htmlFor="idEmail">Email</label>
+                            <input type='email' name='email' id='idEmail' placeholder='seuemail@gmail.com' onChange={handleChange} />
+                        </div>
+
+                        <div>
+                            <label htmlFor="idSenha">Senha</label>
+                            <input type='password' name='password' id='idPassword' placeholder='********' onChange={handleChange} />
+                        </div>
+
+                        <div>
+                            <button>LOGIN</button>
+                        </div>
                     </fieldset>
                 </form>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Rotas
+// Importando as Rotas
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './Pages/Login/Login.jsx';
 import Error404 from './Pages/Error404/Error404.jsx';
 import Home from './Pages/Home/Home.jsx';
 
+// Criando as Rotas
 const router = createBrowserRouter([
   {path:'/', element:<App />, errorElement:<Error404 />,
     children:[
@@ -18,7 +18,5 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <RouterProvider router={router}/>
-  </React.StrictMode>,
 )
