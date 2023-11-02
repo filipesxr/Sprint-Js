8841291d815e3ea7367e27959518a32b4fc0dfc8
filipesxr <rm97830@fiap.com.br>
2023-11-02 17:20:12 -0300

    Adicionando README.MD

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,23 @@
 export default function Home() {
     return(
         <>
-            <h1>Home</h1>
+            <h1>Seja bem-vindo ao seu Dashboard!</h1>
+
+            <div>
+                <table>
+                    <thead>
+                        <th>sa</th>
+                        <th>sa</th>
+                        <th>sa</th>
+                    </thead>
+                    
+                    <tbody>
+                        <th>sdas</th>
+                        <th>dasa</th>
+                        <th>dss</th>
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
