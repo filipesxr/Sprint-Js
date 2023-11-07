339c0a5d1d9c5a4152c0aaa2b56daa718f3fbd4a
filipesxr <rm97830@fiap.com.br>
2023-11-07 16:24:18 -0300

    Finalização do Projeto

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -1,7 +1,13 @@
+import './Error404.scss'
+
 export default function Error404() {
     return(
         <>
-            <h1>Error404</h1>
+            <div className='error'>
+                <h1>Error404</h1>
+
+                <p>Sentimos muito, algum error inesperado aconteceu!</p>
+            </div> 
         </>
     )
 }
