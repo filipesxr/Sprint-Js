462656b6c95c596bd7e503c1ac43ae9422a4a245
filipesxr <rm97830@fiap.com.br>
2023-11-07 16:19:30 -0300

    Estilização da Página Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,47 @@
+import logoMw from '../../Image/mw.png'
+import './Home.scss'
+
 export default function Home() {
+    const logoAlt = 'Logo MW'
+
     return(
         <>
-            <h1>Seja bem-vindo ao seu Dashboard!</h1>
+            <header className='header-home'>
+                <img src={logoMw} alt={logoAlt} />
+                <h2>Seja bem-vindo ao seu Dashboard!</h2>
+            </header>
 
-            <div>
-                <table>
-                    <thead>
-                        <th>sa</th>
-                        <th>sa</th>
-                        <th>sa</th>
-                    </thead>
-                    
-                    <tbody>
-                        <th>sdas</th>
-                        <th>dasa</th>
-                        <th>dss</th>
-                    </tbody>
-                </table>
-            </div>
+            <div className='table-home-container'>
+               <table className='table-home'>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Aparelho</th>
+                    <th>Watts</th>
+                    <th>R$</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>LED da Maquete</td>
+                    <td>Fita de LED</td>
+                    <td>0W</td>
+                    <td>R$0,00</td>
+                  </tr>
+                </tbody>
+              </table> 
+
+              <div className='info-table-home'>
+                <header>
+                    <h3>Acumulativo</h3>
+                </header>
+
+                <p>Watts Acumulados: 0W</p>
+                <p>Reais Acumulados: R$0,00</p>
+              </div>
+            </div>      
         </>
     )
 }
